db51909696b52090b58c25ed38e8b100d140d970
yunpeng <1217152527@qq.com>
2019-02-14 11:18:40 +0800

    ;;;;kl;

---
+++ imooc-security-demo/src/main/java/com/imooc/web/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.imooc.web.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.imooc.dto.User;
+
+
+@RestController
+public class UserController {
+
+	@RequestMapping(value = "/user",method = RequestMethod.GET)
+	public List<User> quert(@RequestParam() String userName){
+		List<User> users = new ArrayList<>();
+		users.add(new User());
+		users.add(new User());
+		users.add(new User());
+				
+		return users;
+	}
+	
+	
+}
