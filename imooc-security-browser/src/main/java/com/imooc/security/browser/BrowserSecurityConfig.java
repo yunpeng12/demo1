705637fcf1862e2ddf82c562715eed8f4ff9e570
yunpeng <1217152527@qq.com>
2019-03-13 09:12:45 +0800

    自定义登录页面

--- imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
+++ imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.imooc.security.browser;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,8 +8,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.imooc.security.core.properties.SecurityProperties;
+
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private SecurityProperties securityProperties;
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -19,11 +25,16 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.formLogin()
-		.loginPage("/imooc-signIn.html")
+		.loginPage("/authentication/reuire")
+		.loginProcessingUrl("/authentication/form")
 		.and()
 		.authorizeRequests()
+		.antMatchers("/authentication/reuire",
+				securityProperties.getBrowser().getLoginPage()).permitAll()
 		.anyRequest()
-		.authenticated();
+		.authenticated()
+		.and()
+		.csrf().disable();
 		
 		
 	}
